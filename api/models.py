4143f2feddb5b274897801cc8da8d87655e2332a
Mustafa Ahmed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-09-10 23:40:19 +0300

    add review for Rater model

--- api/models.py
+++ api/models.py
@@ -13,6 +13,8 @@ class Meal(models.Model):
 class Rater(models.Model):
     meal  = models.ForeignKey(Meal , on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ##add comment on meal 
+    review = models.TextField(blank=True,null=True,max_length=250)
     stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     def __str__(self):
         return f'{self.user} rate {self.meal}'    
